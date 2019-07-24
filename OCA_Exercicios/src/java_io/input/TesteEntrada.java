bac2ee14f7de819b3914165ff7af0022ed1f16f9
Wellington Camilo <wel.camilo10@gmail.com>
2019-07-23 23:07:15 -0300

    Ajustes de Exceptions | Novos Writer Reader

--- OCA_Exercicios/src/java_io/input/TesteEntrada.java
+++ OCA_Exercicios/src/java_io/input/TesteEntrada.java
@@ -6,31 +6,37 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class TesteEntrada {
-	
-	public static void main(String[] args) throws IOException {
-		
-		//Leitor de bytes
-//		InputStream is = new FileInputStream("/home/wellington/Documents/teste.well"); // LENDO A PARTIR DE UM ARQUIVO
-		InputStream is = System.in; // LENDO A PARTIR DE ENTRADA DE DADOS
-		int a = is.read();
-		
-		//Leitor de chars
-		InputStreamReader isr = new InputStreamReader(is);
-		int b = isr.read();
-		
-		//Leitor de Strings
-		BufferedReader br = new BufferedReader(isr);
-		String c = br.readLine();
-		
-		
-		System.out.println("InputStream: " + a);
-		System.out.println("InputStreamReader: " + b);
-		
-		while(c != null) {
-			System.out.println("BufferedReader: " + c);
-			c = br.readLine();
+
+	public static void main(String[] args) {
+
+		try {
+
+			//Leitor de bytes
+//			InputStream is = new FileInputStream("/home/wellington/Documents/teste.well"); // LENDO A PARTIR DE UM ARQUIVO
+			InputStream is = System.in; // LENDO A PARTIR DE ENTRADA DE DADOS
+			int a = is.read();
+
+			//Leitor de chars
+			InputStreamReader isr = new InputStreamReader(is);
+			int b = isr.read();
+
+			//Leitor de Strings
+			BufferedReader br = new BufferedReader(isr);
+			String c = br.readLine();
+
+
+			System.out.println("InputStream: " + a);
+			System.out.println("InputStreamReader: " + b);
+
+			while(c != null) {
+				System.out.println("BufferedReader: " + c);
+				c = br.readLine();
+			}
+			br.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		br.close();
 	}
-	
+
 }
