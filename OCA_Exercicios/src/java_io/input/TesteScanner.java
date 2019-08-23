5adccf6e1491a6c533a0515c25b63deb84b4957e
Wellington Camilo <wellington.camilo@discover.com.br>
2019-08-23 18:14:14 -0300

    Teste com Scanner

--- OCA_Exercicios/src/java_io/input/TesteScanner.java
+++ OCA_Exercicios/src/java_io/input/TesteScanner.java
@@ -11,7 +11,15 @@ public class TesteScanner {
 		Scanner s = new Scanner(new File("contas.csv"));
 		
 		while(s.hasNext()) {
-			System.out.println(s.nextLine());
+			System.out.println(s.nextLine().split(",")[3]);
+			
+			/*
+			 * A linha acima é o mesmo que:
+			 * 
+			 *  String linha = s.nextLine();
+			 *  String [] valores = linha.split(",");
+			 *  System.out.println(valores[3]);
+			 */
 		}
 		
 		s.close();
